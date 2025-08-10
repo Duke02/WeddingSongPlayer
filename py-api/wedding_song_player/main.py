2d955a61a0b944b41d861a4a6da1c1633db831fa
Trystan May <trystanmay@gmail.com>
2025-08-10 16:10:04 -0400

    Added dotenv

---
+++ py-api/wedding_song_player/main.py
@@ -0,0 +1,12 @@
+import typing as tp
+
+from dotenv import load_dotenv
+from fastapi import FastAPI
+
+from wedding_song_player.routes.spotify import spotify_router
+
+load_dotenv()
+
+app = FastAPI()
+
+app.include_router(spotify_router)
